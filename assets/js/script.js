d0918aa5480a336f9bb6f513ade84a13d3a549d3
Nora Dahlberg <ndahlberg95@gmail.com>
2021-03-05 15:50:37 -0700

    finish

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,29 +9,28 @@ var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
 //How long do you want your password to be? (8-128 characters)
 
 //What type of characters do you want your password to have? (uppercase/lowercase letters, numbers, special characters)
-function generatePassword() {
-  console.log("Hey there!");
+function generatePassword() { 
 
   var length = parseInt(
     prompt('How many characters would you like your password to contain?')
   );
 
-  var numericCharacters = confirm (
+  var numericCharacters = confirm(
     "Click 'ok' to include numbers in your password or 'cancel' to exclude numbers."
   );
 
-  var lowercaseCharacters = confirm (
+  var lowercaseCharacters = confirm(
     "Click 'ok' to include lowercase letters in your password or 'cancel' to exclude lowercase letters."
   );
 
-  var uppercaseCharacters  = confirm (
+  var uppercaseCharacters = confirm(
     "Click 'ok' to include uppercase letters in your password or 'cancel' to exclude uppercase letters."
   );
 
-  var specialCharacters  = confirm (
+  var specialCharacters = confirm(
     "Click 'ok' to include special characters in your password or 'cancel' to exclude special characters."
   );
-  
+
   //check options
   if (length < 8) {
     alert('Password length must be at least 8 characters');
@@ -45,31 +44,34 @@ function generatePassword() {
 
   var randomPassword = ""
 
-    if (numericCharacters === true) {
-      randomPassword = randomPassword + numeric;
-    }
-    console.log (randomPassword)
-    if (lowercaseCharacters === true) {
-      randomPassword = randomPassword + lowercase;
-    }
-    console.log (randomPassword)
-    if (uppercaseCharacters === true) {
-      randomPassword = randomPassword + uppercase;
-    }
-    console.log (randomPassword)
-    if (specialCharacters === true) {
-      randomPassword = randomPassword + punctuation;
-    }
-    console.log (randomPassword)
-    
-    for (var i = 0; i < 10; i ++) {
-      console.log ("here's your password")  
-    }
+  var characterOptions = ""
 
-    return randomPassword;
+  if (numericCharacters === true) {
+    characterOptions = characterOptions + numeric;
   }
 
+  if (lowercaseCharacters === true) {
+    characterOptions = characterOptions + lowercase;
+  }
+ 
+  if (uppercaseCharacters === true) {
+    characterOptions = characterOptions + uppercase;
+  }
   
+  if (specialCharacters === true) {
+    characterOptions = characterOptions + punctuation;
+  }
+  
+  for (var i = 0; i < length; i++) {
+    var randIndex = Math.floor (Math.random() * characterOptions.length);
+    var randomChar = characterOptions.charAt(randIndex)
+    randomPassword = randomPassword + randomChar
+  }
+
+  return randomPassword;
+}
+
+
 //DON'T CHANGE ANYTHING BELOW THIS LINE!!!
 
 
